61c1a30bb936829eef5b8068aba6c956421d8875
sist <sist@DESKTOP-GASVUS9>
2019-08-26 14:57:41 +0900

    커밋

--- eHRGit0208/src/eHRGit0208/Hello127.java
+++ eHRGit0208/src/eHRGit0208/Hello127.java
@@ -3,7 +3,9 @@ package eHRGit0208;
 public class Hello127 {
 
 	public static void main(String[] args) {
-		System.out.println("바보~~~");
+		System.out.println("혜준이바보");
+		System.out.println("오늘의 일기");
+		System.out.println("유하언니가... 바보라고 했다... ");
 	}
 
 }
